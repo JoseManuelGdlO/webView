53b8afa55c5dc5b7198dcdeada92c899916e4def
Jose Manuel Garcia de la O <josemanuelgarciadelao@MacBook-Air-de-Jose.local>
2019-04-25 12:11:35 -0500

    cambios grandes

--- src/app/fcm.service.ts
+++ src/app/fcm.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Platform } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FcmService {
 
-  constructor() { }
+  constructor(
+    public firebaseNative: Firebase,
+    public afs: AngularFirestore,
+    private platform: Platform
+  ) { }
+
+  revisar(){
+    
+    this.platform.ready().then(()=>{
+      this.getToken();
+    })
+  }
+
+  async getToken() {
+
+
+    
+
+    let token;
+    
+    alert ("get Token");
+    if (this.platform.is('android')) {
+      token = await this.firebaseNative.getToken()
+    } else if(this.platform.is('ios')) {
+      
+      alert("ios");
+      this.firebaseNative.getToken()
+      .then(tokenpreuba => alert('The token is '+tokenpreuba)) // save the token server-side and use it to push notifications to this device
+      .catch(error => alert('Error getting token'+error));
+      
+      await this.firebaseNative.grantPermission();
+    } 
+    
+    return this.saveTokenToFirestore(token)
+  }
+
+
+  private saveTokenToFirestore(token) {
+    if (!token) return;
+
+    alert(token+"   Toke!");
+  
+    const devicesRef = this.afs.collection('devices')
+  
+    const docData = { 
+      token,
+      userId: 'testUser',
+    }
+  
+    return devicesRef.doc(token).set(docData)
+  }
+
+  listenToNotifications() {
+    return this.firebaseNative.onNotificationOpen()
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { FcmService } from '../fcm.service';
+
+import { ToastController } from '@ionic/angular';
+import { tap } from 'rxjs/operators';
 
 
 @Component({
@@ -11,10 +15,40 @@ export class HomePage {
 
   url: any;
 
-  constructor(private sanitize: DomSanitizer){
+  constructor(
+    private sanitize: DomSanitizer,
+    public fcm: FcmService,
+    public toastCtrl: ToastController
+    ){
 
     this.url = sanitize.bypassSecurityTrustResourceUrl("http://pruebas.inovatix.com.mx/");
 
+    this.ionViewDidLoad();
+
+  }
+
+  ionViewDidLoad(){
+
+    alert("Entrate al metodo");
+
+    //Traer lo de FCM token
+    this.fcm.revisar();
+
+    this.fcm.listenToNotifications().pipe(
+      tap(msg => {
+
+        this.presentToast(msg.body);
+        
+      })
+    );
+  }
+
+  async presentToast(body:string) {
+    const toast = await this.toastCtrl.create({
+      message: body,
+      duration: 3000
+    });
+    toast.present();
   }
 
 }
